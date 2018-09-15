334a7c5def2b6f36c761c8e2fef367fbf7365983
Sompob Chanintornpipat <sompob.chanintornpipat@gmail.com>
2018-09-15 17:04:20 +0700

    fix webMvcTest by Stub

--- src/test/java/com/lotto/lotto/controller/AccountControllerWebMvcTest.java
+++ src/test/java/com/lotto/lotto/controller/AccountControllerWebMvcTest.java
@@ -2,18 +2,24 @@ package com.lotto.lotto.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lotto.lotto.controller.response.AccountResponse;
+import com.lotto.lotto.model.Account;
+import com.lotto.lotto.repository.AccountRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Optional;
+
 import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @RunWith(SpringRunner.class)
@@ -23,12 +29,23 @@ public class AccountControllerWebMvcTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private AccountRepository accountRepository;
+
     private JacksonTester<AccountResponse> jsonTester;
 
     @Test
     public void getById() throws Exception {
 
         JacksonTester.initFields(this, new ObjectMapper());
+
+        //Stub
+        Account account = new Account();
+        account.setUserName("user");
+        account.setPassword("pass");
+        account.setSalary(1000);
+        given(accountRepository.findById(1))
+                .willReturn(Optional.of(account));
 
         MockHttpServletResponse response
                 = mockMvc.perform(
