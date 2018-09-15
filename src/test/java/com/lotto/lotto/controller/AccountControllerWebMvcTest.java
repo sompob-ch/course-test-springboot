fb50f7502c05e43f23a9839c02a0212841cac887
Sompob Chanintornpipat <sompob.chanintornpipat@gmail.com>
2018-09-15 14:54:36 +0700

    add test WebMvcTest

---
+++ src/test/java/com/lotto/lotto/controller/AccountControllerWebMvcTest.java
@@ -0,0 +1,46 @@
+package com.lotto.lotto.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lotto.lotto.controller.response.AccountResponse;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(AccountController.class)
+public class AccountControllerWebMvcTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private JacksonTester<AccountResponse> jsonTester;
+
+    @Test
+    public void getById() throws Exception {
+
+        JacksonTester.initFields(this, new ObjectMapper());
+
+        MockHttpServletResponse response
+                = mockMvc.perform(
+                get("/account/1")
+                        .accept(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        assertEquals("For message error EX. Status should 200", HttpStatus.OK.value(), response.getStatus());
+
+        AccountResponse expected
+                = new AccountResponse("user","pass",1000);
+        assertEquals("XXX", jsonTester.write(expected).getJson(),
+                response.getContentAsString());
+    }
+}
